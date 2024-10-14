1bdd9401a3ae2d207ab269ce6359ec36980e69d4
Sapan Gupta <sapan.gupta1982@gmail.com>
2024-10-14 22:45:35 +0530

    updated code

--- app.py
+++ app.py
@@ -120,5 +120,7 @@ def logout():
     
     return render_template('login.html')
 
-
-app.run(debug=True)
+if __name__=='__main__':
+    #app.run(host='127.0.0.1',port=8080,debug=True)
+    app.run(host='0.0.0.0',port=8080)
+    
