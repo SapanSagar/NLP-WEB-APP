199cdf81211d39bc694d95f0844ea479c7c186cb
Sapan Gupta <sapan.gupta1982@gmail.com>
2024-10-15 22:05:44 +0530

    updated main.yaml

--- app.py
+++ app.py
@@ -121,6 +121,6 @@ def logout():
     return render_template('login.html')
 
 if __name__=='__main__':
-    #app.run(host='127.0.0.1',port=8080,debug=True)
-    app.run(host='0.0.0.0',port=8080)
+    app.run(host='127.0.0.1',port=8080,debug=True)
+    #app.run(host='0.0.0.0',port=8080)
     
